8ecff3f3be9755976f526617c8ee4ff9ccedbefd
Archichil <kuhockovolec@gmail.com>
2025-03-14 20:19:54 +0300

    Project init

---
+++ UniWeather/UniWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniWeatherApp.swift
+//  UniWeather
+//
+//  Created by Archichil on 14.03.25.
+//
+
+import SwiftUI
+
+@main
+struct UniWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
