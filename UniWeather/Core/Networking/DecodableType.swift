6ea74d6c406c53c12c8ef71afe100dd1f003013f
Archichil <kuhockovolec@gmail.com>
2025-03-18 00:26:25 +0300

    Documentation for some networking code + added ai api key field in the config

--- UniWeather/Core/Networking/DecodableType.swift
+++ UniWeather/Core/Networking/DecodableType.swift
@@ -5,7 +5,31 @@
 //  Created by Daniil on 15.03.25.
 //
 
+/// A protocol that marks a type as decodable from JSON data.
+///
+/// This protocol is used to ensure that types can be decoded from JSON responses
+/// returned by the API. It is a refinement of the `Decodable` protocol.
+///
+/// ## Example
+/// ```swift
+/// struct MyResponse: DecodableType {
+///     let id: Int
+///     let name: String
+/// }
+/// ```
 protocol DecodableType: Decodable { }
-// Make Array of DecodableType also conform to DecodableType if needed.
-// For cases where the API returns an array of entities.
+
+/// Extends `Array` to conform to `DecodableType` when its elements conform to `DecodableType`.
+///
+/// This allows arrays of decodable types to be used as return types in API specifications.
+///
+/// ## Example
+/// ```swift
+/// struct MyResponse: DecodableType {
+///     let id: Int
+///     let name: String
+/// }
+///
+/// let response: [MyResponse] = try decoder.decode([MyResponse].self, from: data)
+/// ```
 extension Array: DecodableType where Element: DecodableType { }
