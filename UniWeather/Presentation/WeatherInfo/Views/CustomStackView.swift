5fabf4cbdeab90fd90d2701611bc0f227b7e7520
Archichil <kuhockovolec@gmail.com>
2025-05-06 01:48:56 +0300

    improved CustomStackView structure by adding constants

--- UniWeather/Presentation/WeatherInfo/Views/CustomStackView.swift
+++ UniWeather/Presentation/WeatherInfo/Views/CustomStackView.swift
@@ -13,6 +13,11 @@ struct CustomStackView<Title: View, Content: View >: View {
     @State var topOffset: CGFloat = 0
     @State var bottomOffset: CGFloat = 0
     
+    // MARK: - Constants
+    let sectionTopSafeArea: CGFloat = 145
+    let titleHeight: CGFloat = 38
+    let cornerRadius: CGFloat = 12
+    
     init(@ViewBuilder titleView: @escaping () -> Title, @ViewBuilder contentView: @escaping () -> Content) {
         self.titleView = titleView()
         self.contentView = contentView()
@@ -26,9 +31,9 @@ struct CustomStackView<Title: View, Content: View >: View {
                 .bold()
                 .foregroundStyle(.secondary)
                 .lineLimit(1)
-                .frame(height: 38)
+                .frame(height: titleHeight)
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .background(.ultraThinMaterial, in: CustomCorner(corners: bottomOffset < 38 ? .allCorners : [.topLeft, .topRight], radius: 12))
+                .background(.ultraThinMaterial, in: CustomCorner(corners: bottomOffset < titleHeight ? .allCorners : [.topLeft, .topRight], radius: cornerRadius))
                 .zIndex(1)
             
             VStack(spacing: 0) {
@@ -38,15 +43,15 @@ struct CustomStackView<Title: View, Content: View >: View {
                     .padding(.bottom)
                     .padding(.top, 12)
             }
-            .background(.ultraThinMaterial, in: CustomCorner(corners: [.bottomLeft, .bottomRight], radius: 12))
-            .offset(y: topOffset >= 145 ? 0 : -(-topOffset + 145))
+            .background(.ultraThinMaterial, in: CustomCorner(corners: [.bottomLeft, .bottomRight], radius: cornerRadius))
+            .offset(y: topOffset >= sectionTopSafeArea ? 0 : -(-topOffset + sectionTopSafeArea))
             .zIndex(0)
             .clipped()
             .opacity(getOpacity())
         }
-        .clipShape(.rect(cornerRadius: 12))
+        .clipShape(.rect(cornerRadius: cornerRadius))
         .opacity(getOpacity())
-        .offset(y: topOffset >= 145 ? 0 : -topOffset + 145)
+        .offset(y: topOffset >= sectionTopSafeArea ? 0 : -topOffset + sectionTopSafeArea)
         .background {
             GeometryReader { proxy -> Color in
                 let minY = proxy.frame(in: .global).minY
@@ -54,7 +59,7 @@ struct CustomStackView<Title: View, Content: View >: View {
                 
                 DispatchQueue.main.async {
                     self.topOffset = minY
-                    self.bottomOffset = maxY - 145
+                    self.bottomOffset = maxY - sectionTopSafeArea
                 }
                 return Color.clear
             }
@@ -74,13 +79,15 @@ struct CustomStackView<Title: View, Content: View >: View {
 
 struct CornerModifier: ViewModifier {
     @Binding var bottomOffset: CGFloat
+    let titleHeight: CGFloat = 38
+    let cornerRadius: CGFloat = 12
     
     func body(content: Content) -> some View {
-        if bottomOffset < 38 {
+        if bottomOffset < titleHeight {
             content
         } else {
             content
-                .clipShape(.rect(cornerRadius: 12))
+                .clipShape(.rect(cornerRadius: cornerRadius))
         }
     }
 }
