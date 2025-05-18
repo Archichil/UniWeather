7a707cb196f13a22510a0590c617d00228c70535
Archichil <kuhockovolec@gmail.com>
2025-05-18 15:13:55 +0300

    fix: Changed deployment targets

--- WeatherMapService/Package.swift
+++ WeatherMapService/Package.swift
@@ -1,13 +1,11 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "WeatherMapService",
-    platforms: [.iOS(.v17)],
+    platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WeatherMapService",
             targets: ["WeatherMapService"]
@@ -17,8 +15,6 @@ let package = Package(
         .package(name: "WeatherService", path: "../WeatherService"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "WeatherMapService",
             dependencies: ["WeatherService"]
